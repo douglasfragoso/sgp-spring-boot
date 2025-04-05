bf7dd22a8e3efc4a6af0549a43f92565df355abe
Douglas Fragoso <douglas.iff@gmail.com>
2025-04-04 22:46:39 -0300

    docs: target

--- src/test/java/com/tarefas/api/TarefasApiApplicationTests.java
+++ src/test/java/com/tarefas/api/TarefasApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.tarefas.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  
 class TarefasApiApplicationTests {
 
 	@Test
